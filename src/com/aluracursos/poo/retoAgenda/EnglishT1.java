abdf051037b4323061d4e6f22a9e9aa88f3f4ae5
MALOOR <maurizio10e@gmail.com>
2024-08-06 22:40:02 -0600

    homework

---
+++ src/com/aluracursos/poo/retoAgenda/EnglishT1.java
@@ -0,0 +1,22 @@
+package com.aluracursos.poo.retoAgenda;
+
+public class EnglishT1 {
+
+
+    //This session is a homework of the service in the mediateca of ENALLT.
+    /*
+    The mediateca
+    A hub with resources to learn and practice English, Chinese, French, German, Italian, Japanese, Portuguese, and Russian.
+    In the Media Library you can learn at your own pace and according to your needs, goals and personal possibilities.
+    Various academic supports are provided to develop the skills that favor the language learning process.
+     */
+
+    /*
+
+    What is the purpose of the academic support of the Media Library?
+    • To complement your study with more practice, reinforcement and correction of errors.
+     Mainly for those who study German, Chinese, French, English, Italian, Japanese, Portuguese and Russian at the ENALLT Language Center
+     • To learn or improve the language in a global way: or on specific language skills such as reading comprehension, listening comprehension, oral and written expression, among others, in this way the Media Library is the alternative modality to the classroom.
+      • To prepare for international certifications (TOEFL, TOEFL iBT, IELTS, DELF, DALF, ZD, ÖSD, Mittelstufe Deutsch, HSK, JLPT or CELI, among others) or an exam to meet the degree requirements in UNAM schools or faculties. The Media Library contains academic support materials and services for these specific needs.
+     */
+}
